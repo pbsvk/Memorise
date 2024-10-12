e994a6e172fb467a00af8ff638b00fd6971478b2
pbsvk <152455231+pbsvk@users.noreply.github.com>
2024-10-12 11:25:30 -0400

    Initial Commit

---
+++ Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by Bhaskara Sai Vamsi Krishna Padala on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PracticeTests/PracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeTests.swift
+//  PracticeTests
+//
+//  Created by Bhaskara Sai Vamsi Krishna Padala on 10/12/24.
+//
+
+import Testing
+@testable import Practice
+
+struct PracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
