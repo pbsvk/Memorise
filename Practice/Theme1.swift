97670b0f63f7e45e6cdd106cee0d349ad7447884
pbsvk <152455231+pbsvk@users.noreply.github.com>
2024-10-14 19:46:10 -0400

    Assignment_1

---
+++ Practice/Theme1.swift
@@ -0,0 +1,18 @@
+//
+//  Theme1.swift
+//  Practice
+//
+//  Created by Bhaskara Sai Vamsi Krishna Padala on 10/13/24.
+//
+
+import SwiftUI
+
+struct Theme1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Theme1()
+}
